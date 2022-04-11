fea8408544524c3c37913e1c87e5504ad6f091c6
AndersonAndrad <anderson_andrade_@outlook.com>
2022-04-11 01:02:40 -0300

    test: ensure SingUpController returns 400 if no password is provided

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -35,4 +35,21 @@ describe( 'SingUp controller', () => {
     expect( httpResponse?.statusCode ).toBe( 400 )
     expect( httpResponse?.body ).toEqual( new MissingParamError( 'email' ) )
   } )
+
+  test( 'Should return 400 if no name is provied', () => {
+    const sut = new SingUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'anderson',
+        email: 'anderson@email.com',
+        passwordConfirmation: '123456'
+      }
+    }
+
+    const httpResponse = sut.handle( httpRequest )
+
+    expect( httpResponse?.statusCode ).toBe( 400 )
+    expect( httpResponse?.body ).toEqual( new MissingParamError( 'password' ) )
+  } )
 } )
