b43c61deacd75e5f3304257ee0dac5c6945efb61
AndersonAndrad <anderson_andrade_@outlook.com>
2022-04-21 14:09:12 -0300

    test: ensure SingUpController returns 500 if AddAccounts throws

--- src/presentation/controllers/singup/singup.spec.ts
+++ src/presentation/controllers/singup/singup.spec.ts
@@ -176,6 +176,28 @@ describe( 'SingUp controller', () => {
     expect( httpResponse?.body ).toEqual( new ServerError() )
   } )
 
+  test( 'Should return 500 if AddAccount throws ', () => {
+    const { sut, addAccountStub } = makeSut()
+
+    jest.spyOn( addAccountStub, 'add' ).mockImplementationOnce( () => {
+      throw new Error()
+    } )
+
+    const httpRequest = {
+      body: {
+        name: 'anderson',
+        email: 'invalid@email.com',
+        password: '123456',
+        passwordConfirmation: '123456'
+      }
+    }
+
+    const httpResponse = sut.handle( httpRequest )
+
+    expect( httpResponse?.statusCode ).toBe( 500 )
+    expect( httpResponse?.body ).toEqual( new ServerError() )
+  } )
+
   test( 'Should call EmailValidator with correct email', () => {
     const { sut, emailValidatorStub } = makeSut()
 
